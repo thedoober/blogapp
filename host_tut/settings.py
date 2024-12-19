95bea0268f5d6f2b356a0e5e8df6793218f69acc
Vivek Anand <thevivek9939@gmail.com>
2024-12-19 18:54:46 +0530

    Initial commit

--- host_tut/settings.py
+++ host_tut/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bmqjt43$p($)qheflo_ni6kg-_zmwoe0c@y3k5#p%6a%+x64&d'
+SECRET_KEY = os.getenv('SECRET_KEY', 'Render766@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
